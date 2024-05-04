7191218901196b4691f5be4f493668e297cbf019
pisboiii <hfzfahrzi@gmail.com>
2024-05-04 10:02:56 -0700

    membuat void delete addnode

--- 181_DoubleLinkedList.cpp
+++ 181_DoubleLinkedList.cpp
@@ -73,7 +73,7 @@ bool search(int rollNo, Node** previous, Node** current)
     return (*current != NULL);
 }
 
-void deleteNode()
+void deleteNode() 
 {
     Node* previous, * current;
     int rollNo;
