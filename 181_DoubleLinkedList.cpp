be5f5df679130a31b9ba012e0c71bc2d8d6e64ce
pisboiii <hfzfahrzi@gmail.com>
2024-05-04 10:04:51 -0700

    membuat bool list empty

--- 181_DoubleLinkedList.cpp
+++ 181_DoubleLinkedList.cpp
@@ -124,7 +124,7 @@ void deleteNode()
     //release the memory of the node marked as current
     delete current;
     cout << "\x1b[32mRecord with roll number " << rollNo << "deleted\x1b[0m" << endl;
-}
+} 
    
 bool listEmpty()
 {
